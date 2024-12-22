842f25991ec097f3ca82eff9b96a5d8a4143cbb2
Jakub Bytow <jakub.bytow@gmail.com>
2024-12-22 13:26:40 +0100

    project structure change

--- src/main/java/com/example/coffeetica/coffee/repositories/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.coffeetica.coffee.repositories;
-
-import com.example.coffeetica.coffee.models.ReviewEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-
-}
--- src/main/java/com/example/coffeetica/user/services/impl/RoleServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.example.coffeetica.user.services.impl;
-
-public class RoleServiceImpl {
-}
