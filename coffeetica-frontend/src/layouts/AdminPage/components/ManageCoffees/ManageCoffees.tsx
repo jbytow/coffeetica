9c652cac59150b063619052fc84e9abea66415a9
Jakub Bytow <jakub.bytow@gmail.com>
2025-01-18 12:31:31 +0100

    ManageCoffees.tsx styling

--- coffeetica-frontend/src/layouts/AdminPage/components/ManageCoffees/ManageCoffees.tsx
+++ coffeetica-frontend/src/layouts/AdminPage/components/ManageCoffees/ManageCoffees.tsx
@@ -35,45 +35,95 @@ const ManageCoffees: React.FC = () => {
 
   return (
     <div>
-      <h2>Manage Coffees</h2>
       {error && <p className="text-danger">{error}</p>}
-      <div className="mb-3">
-        <Link to="/admin/coffees/add" className="btn btn-primary">
+      <div className="mb-3 d-flex align-items-center">
+        <Link to="/admin/coffees/add" className="btn btn-primary me-3">
           Add New Coffee
         </Link>
       </div>
-      <div>
-        <h3>Existing Coffees</h3>
-        <ul>
-          {coffees.map((coffee) => (
-            <li key={coffee.id}>
-              <strong>{coffee.name}</strong> <br />
-              Roast Level: {coffee.roastLevel} <br />
-              {coffee.imageUrl && (
-                <img
-                  src={`http://localhost:8080${coffee.imageUrl}`}
-                  alt={`${coffee.name} image`}
-                  width="100"
-                />
-              )}
-              <div className="mt-2">
-                <button
-                  className="btn btn-danger me-2"
-                  onClick={() => handleDeleteCoffee(coffee.id)}
-                >
-                  Delete
-                </button>
-                <Link
-                  to={`/admin/coffees/edit/${coffee.id}`}
-                  className="btn btn-secondary"
-                >
-                  Edit
-                </Link>
+      {coffees.map((coffee) => (
+        <div className="card mt-3 shadow p-3 mb-3 bg-body rounded" key={coffee.id}>
+          <div className="row g-0">
+            {/* Obraz */}
+            <div className="col-md-3 d-flex justify-content-center align-items-center">
+              <div>
+                {coffee.imageUrl ? (
+                  <img
+                    src={`http://localhost:8080${coffee.imageUrl}`}
+                    width="123"
+                    height="196"
+                    alt={coffee.name}
+                  />
+                ) : (
+                  <img
+                    src="https://via.placeholder.com/123x196"
+                    width="123"
+                    height="196"
+                    alt="Coffee placeholder"
+                  />
+                )}
               </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+            {/* Szczegóły kawy */}
+            <div className="col-md-9">
+              <div className="card-body">
+                <div className="row align-items-center mb-2">
+                  <div className="col-md-5">
+                    <h5 className="card-title fs-4">{coffee.name}</h5>
+                  </div>
+                  <div className="col-md-5 d-flex justify-content-start">
+                    <Link
+                      to={`/admin/coffees/edit/${coffee.id}`}
+                      className="btn btn-secondary me-2"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDeleteCoffee(coffee.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+                <div className="row">
+                  {/* Kolumna 1 */}
+                  <div className="col-md-5">
+                    <p className="card-text">
+                      <strong>Roastery:</strong> {coffee.roastery.name}
+                    </p>
+                    <p className="card-text">
+                      <strong>Region:</strong> {coffee.region}
+                    </p>
+                    <p className="card-text">
+                      <strong>Country of Origin:</strong> {coffee.countryOfOrigin}
+                    </p>
+                    <p className="card-text">
+                      <strong>Production Year:</strong> {coffee.productionYear}
+                    </p>
+                  </div>
+                  {/* Kolumna 2 */}
+                  <div className="col-md-5">
+                    <p className="card-text">
+                      <strong>Roast Level:</strong> {coffee.roastLevel}
+                    </p>
+                    <p className="card-text">
+                      <strong>Flavor Profile:</strong> {coffee.flavorProfile}
+                    </p>
+                    <p className="card-text">
+                      <strong>Flavor Notes:</strong>{" "}
+                      {coffee.flavorNotes.join(", ")}
+                    </p>
+                    <p className="card-text">
+                      <strong>Processing Method:</strong> {coffee.processingMethod}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
