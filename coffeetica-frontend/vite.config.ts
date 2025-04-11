b744569281c01846337a67be5c9ace8d7f9738b8
Jakub Bytow <jakub.bytow@gmail.com>
2025-04-11 13:39:06 +0200

    frontend and docker-compose config to use variable

--- coffeetica-frontend/vite.config.ts
+++ coffeetica-frontend/vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-
-    define: {
-      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+})
