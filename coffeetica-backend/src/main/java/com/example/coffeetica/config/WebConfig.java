ec7e9de7917b668408e9712c24879310b043a3a5
Jakub Bytow <jakub.bytow@gmail.com>
2025-04-12 13:45:29 +0200

    static file fix, removed file serve from spring WebConfig

--- coffeetica-backend/src/main/java/com/example/coffeetica/config/WebConfig.java
+++ coffeetica-backend/src/main/java/com/example/coffeetica/config/WebConfig.java
@@ -18,10 +18,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
-    }
 }
